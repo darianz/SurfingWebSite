45a05c679b2fc5181dd7582a181502ff98181fe8
idan <idan.avin@gmail.com>
2019-06-01 21:21:41 +0300

    updated social-icons

--- src/app/social-icons/social-icons.component.ts
+++ src/app/social-icons/social-icons.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { delay } from 'q';
 
 @Component({
   selector: 'app-social-icons',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./social-icons.component.scss']
 })
 export class SocialIconsComponent implements OnInit {
+
+  onMouse (iconImageElement, color) {
+    const enteredElement = document.querySelectorAll(`.icon__img--${iconImageElement}`);
+
+    setTimeout(function () {
+      enteredElement.forEach(function (el) {
+        el.setAttribute('style', `fill:${color} !important`);
+      })
+    }, 500)
+  }
 
   constructor() { }
 
