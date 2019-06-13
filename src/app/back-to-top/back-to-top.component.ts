f8bfc926377037d432686b0a5c9a0a4de453ef1b
ros stolero <ronst@ac.sce.ac.il>
2019-06-13 19:28:13 +0300

    recommendations left and right buttons

--- src/app/back-to-top/back-to-top.component.ts
+++ src/app/back-to-top/back-to-top.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, HostListener } from '@angular/core';
+import { DOCUMENT } from "@angular/platform-browser";
+
 
 @Component({
   selector: 'app-back-to-top',
   templateUrl: './back-to-top.component.html',
   styleUrls: ['./back-to-top.component.scss']
 })
+
 export class BackToTopComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  windowScrolled: boolean;
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+      if (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
+          this.windowScrolled = true;
+      } 
+     else if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) {
+          this.windowScrolled = false;
+      }
   }
+  scrollToTop() {
+      (function smoothscroll() {
+          var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+          if (currentScroll > 0) {
+              window.requestAnimationFrame(smoothscroll);
+              window.scrollTo(0, currentScroll - (currentScroll / 8));
+          }
+      })();
+  }
+  ngOnInit() {}
 
 }
+
+
+// back-to-top
--- src/app/recommendations/recommendations.component.ts
+++ src/app/recommendations/recommendations.component.ts
@@ -10,7 +10,9 @@ export class RecommendationsComponent implements OnInit {
 
   example1 = new cardModel('example1', '../../assets/Shta.jpg', 'dummy text 1','dwad');
   example2 = new cardModel('example2', '../../assets/Shta.jpg', 'dummy text 2','dwad');
-  recommendations = [this.example1, this.example2]; 
+  example3 = new cardModel('example3', '../../assets/Shta.jpg', 'dummy text 3','dwad');
+  example4 = new cardModel('example4', '../../assets/Shta.jpg', 'dummy text 4','dwad');
+  recommendations = [this.example1, this.example2, this.example3, this.example4]; 
    
   constructor() {
 
