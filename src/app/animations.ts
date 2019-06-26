29716fa6f0b44a067616ef221c36c26801aacfcd
ros stolero <ronst@ac.sce.ac.il>
2019-06-26 12:44:49 +0300

    started the animations on recommendations component

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -8,5 +8,41 @@ export let navButtonShow= trigger('navButtonShow', [
         animate(2000)
       ])
     ]);
-  
+
+
+    export let fadeIn= trigger('fadeIn', [
+
+      state('void', style ({ opacity: 0 })),
+
+      transition('void <=> *', [
+        animate(2000)
+      ]),
+
+    ]);
+
+    export let leftToRight= trigger('leftToRight', [
+
+      state('*',style({})),
+
+      transition('* <=> *', [
+        animate(2000)
+      ]),
+
+    ]);
+
+    export let moveState= trigger('moveState', [
+
+      state('leftToRight',style({})),
+
+      transition('none => leftToRight', [
+        animate(1000), style({left: '-192px'})
+      ]),
+
+      transition('none => rightToLeft', [
+        animate(1000), style({left: '192px'})
+      ]),
+
+    ]);
+    
+    
 
--- src/app/recommendations/recommendations.component.ts
+++ src/app/recommendations/recommendations.component.ts
@@ -1,19 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import {cardModel} from './cardModel';
+import { trigger, state, transition, style, animate } from '@angular/animations';
+import { fadeIn, leftToRight, moveState } from '../animations';
 
 @Component({
   selector: 'app-recommendations',
   templateUrl: './recommendations.component.html',
-  styleUrls: ['./recommendations.component.scss']
+  styleUrls: ['./recommendations.component.scss'],
+  animations:[
+    fadeIn,
+    leftToRight,
+    moveState
+  ]
 })
 export class RecommendationsComponent implements OnInit {
   obj;
+  stateName = 'none';
   example1 = new cardModel('example1', '../../assets/Shta.jpg', 'dummy text 1','dwad');
   example2 = new cardModel('example2', '../../assets/Shta.jpg', 'dummy text 2','dwad');
   example3 = new cardModel('example3', '../../assets/Shta.jpg', 'dummy text 3','dwad');
   example4 = new cardModel('example4', '../../assets/Shta.jpg', 'dummy text 4','dwad');
   recommendations = [this.example1, this.example2, this.example3, this.example4]; 
-   
+   box;
+   left = '-192px';
   constructor() {
 
     
@@ -24,13 +33,25 @@ export class RecommendationsComponent implements OnInit {
 
   }
   LeftClick() {
-    this.obj = this.recommendations.shift();
-    this.recommendations.push(this.obj);
+  
+    this.stateName = 'leftToRight';
+    setTimeout(()=>{
+      this.obj = this.recommendations.shift();
+      this.recommendations.push(this.obj);
+      this.stateName = 'none';
+    } , 1000);
+
   }
 
   RightClick()  {
-    this.obj = this.recommendations.pop();
-    this.recommendations.unshift(this.obj);
+
+    this.stateName = 'rightToLeft';
+    setTimeout(()=>{
+      this.obj = this.recommendations.pop();
+      this.recommendations.unshift(this.obj);
+      this.stateName = 'none';
+    } , 1000);
+    
   }
 }
 
